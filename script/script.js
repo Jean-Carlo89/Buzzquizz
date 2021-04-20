60ab974f115818b2bca9d650dec613dbc70cf793
Jean Carlo <jeancarlomo@hotmail.com>
2021-04-20 19:04:44 -0300

    implemento do drop nas perguntas

--- script/script.js
+++ script/script.js
@@ -64,3 +64,16 @@ function testToggleHide(clickedButton,classOfButton){
     console.log(divToToggle)
     divToToggle.classList.toggle('hidden')
 }
+
+
+function addDrop(clickedIcon){
+    const addHiddenToOthers = document.querySelectorAll('.question')
+    for(let i =0; i<addHiddenToOthers.length;i++){
+        addHiddenToOthers[i].classList.add('hidden')
+    }
+    console.log(clickedIcon)
+    console.log(clickedIcon.parentNode.nextElementSibling)
+    clickedIcon.parentNode.nextElementSibling.classList.remove('hidden')
+    clickedIcon.parentNode.nextElementSibling.classList.add('drop')
+    clickedIcon.parentNode.nextElementSibling.scrollIntoView()
+}
