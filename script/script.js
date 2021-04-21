83f341c38fa526907e458ccc2a19139eafb3a618
Jean Carlo <jeancarlomo@hotmail.com>
2021-04-21 13:54:51 -0300

    Término dos layouts e melhorias no drop. falta ajustar drop do layput 9

--- script/script.js
+++ script/script.js
@@ -61,7 +61,7 @@
 function testToggleHide(clickedButton,classOfButton){
     //alert('clicado')
     const divToToggle = document.querySelector('.'+classOfButton)
-    console.log(divToToggle)
+   // console.log(divToToggle)
     divToToggle.classList.toggle('hidden')
 }
 
@@ -77,3 +77,19 @@ function addDrop(clickedIcon){
     clickedIcon.parentNode.nextElementSibling.classList.add('drop')
     clickedIcon.parentNode.nextElementSibling.scrollIntoView()
 }
+
+function addDropLevel(clickedIcon){
+    const addHiddenToOthers = document.querySelectorAll('.level-container')
+    for(let i =0; i<addHiddenToOthers.length;i++){
+        addHiddenToOthers[i].classList.add('hidden')
+    }
+    // console.log(clickedIcon)
+    //console.log(clickedIcon.parentNode)
+    //console.log(clickedIcon.parentNode)
+    //console.log(clickedIcon.parentNode.nextElementSibling)
+    //clickedIcon.parentNode.nextElementSibling.classList.remove('hidden')
+    clickedIcon.parentNode.nextElementSibling.classList.remove('hidden')
+    clickedIcon.parentNode.nextElementSibling.classList.add('drop')
+    clickedIcon.parentNode.nextElementSibling.scrollIntoView()
+}
+
